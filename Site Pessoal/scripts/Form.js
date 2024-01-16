4c97de515a144cda8c75e77bd536ca7be1288920
Gustavo <gustavodiassilveira7@gmail.com>
2024-01-15 21:10:52 -0300

    refactor
    Breve refatoração no código, mas sem alterar as funcionalidades.

--- Site Pessoal/scripts/Form.js
+++ Site Pessoal/scripts/Form.js
@@ -1,20 +1,34 @@
-const email = document.getElementById("email").value;
-const telefone = document.getElementById("celular").value;
-const messsage = document.getElementById("mensagem").value;
+// Este código adiciona um evento de clique ao elemento com o id 'emailButton'
+document.getElementById('enviar').addEventListener('click', sendMail);
 
+// Esta é a função que será chamada quando o botão com id 'emailButton' for clicado
+function sendMail (e) {
+    // Previne o comportamento padrão do clique em um botão dentro de um formulário.
+    e.preventDefault();
+    
+    // Obtém os valores dos campos de entrada do formulário com base em seus respectivos IDs.
+    const mail = document.getElementById('email').value;
+    const message = document.getElementById('mensagem').value;
+    const name = document.getElementById('nome').value;
 
-if( email && messsage){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert("Enviado com sucesso.")
-    );
-}else{
-    alert("Preencha os campos!")
+    // Verifica se todos os campos (e-mail, nome e  mensagem) foram preenchidos pelo usuário.
+    if (name && mail && message) {
+        // Se todos os campos estiverem preenchidos, usa a biblioteca 'Email.js'
+        // para enviar o e-mail com os detalhes fornecidos pelo usuário.
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "contatosilveiragustavo@gmail.com",
+            Password : "21112FD548BADA088F1C740B4C961670B556",
+            To : 'gustavodiassilveira7@gmail.com',
+            From : "contatosilveiragustavo@gmail.com",
+            Subject: `Contato via Portfólio`,
+            Body: ` ${message}`,
+        }).then(() => {
+            alert('Obrigado pelo seu contato!');
+            location.reload();
+        });
+    } else {
+        // Se algum dos campos estiver em branco, exibe um alerta informando que todos os campos devem ser preenchidos.
+        alert('Preencha todos os campos!');
+    }    
 }
--- Site Pessoal/scripts/Menu.js
+++ Site Pessoal/scripts/Menu.js
@@ -1,10 +1,16 @@
-let abre_menu =  document.getElementById('abrir-menu');
-let menu =  document.getElementById('menu-mobile');
 
-abre_menu.addEventListener('click', ()=> {
+let btnMenu = document.getElementById('abrir-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-omenu')
 })
 
-menu.addEventListener('click', ()=> {
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-omenu')
+})
+
+overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-omenu')
 })
